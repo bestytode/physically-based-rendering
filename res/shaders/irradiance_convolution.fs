e7edfd67f016a59e98187a58b9328a7f6c9d0f5a
余振寰 <1932878552@qq.com>
2023-10-26 08:24:18 +0100

    1

--- res/shaders/irradiance_convolution.fs
+++ res/shaders/irradiance_convolution.fs
@@ -8,11 +8,9 @@ const float PI = 3.14159265359;
 
 void main()
 {		
-	// The world vector acts as the normal of a tangent surface
-    // from the origin, aligned to WorldPos. Given this normal, calculate all
-    // incoming radiance of the environment. The result of this radiance
-    // is the radiance of light coming from -Normal direction, which is what
-    // we use in the PBR shader to sample irradiance.
+	// The world vector is a surface normal from the origin to WorldPos.
+    // Using this, compute incoming radiance from the environment.
+    // This radiance comes from the -Normal direction, used in the PBR shader for irradiance sampling.
     vec3 N = normalize(WorldPos);
 
     vec3 irradiance = vec3(0.0);   
@@ -22,21 +20,26 @@ void main()
     vec3 right = normalize(cross(up, N));
     up         = normalize(cross(N, right));
        
-    float sampleDelta = 0.025;
+    float sampleDelta = 0.025; 
     float nrSamples = 0.0;
-    for(float phi = 0.0; phi < 2.0 * PI; phi += sampleDelta)
-    {
-        for(float theta = 0.0; theta < 0.5 * PI; theta += sampleDelta)
-        {
-            // spherical to cartesian (in tangent space)
+
+    // Iterate over hemisphere oriented around normal N.
+    for(float phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
+        for(float theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
+            // Convert spherical coordinates to tangent space.
             vec3 tangentSample = vec3(sin(theta) * cos(phi),  sin(theta) * sin(phi), cos(theta));
-            // tangent space to world
+            
+            // Transform sample from tangent space to world space.
             vec3 sampleVec = tangentSample.x * right + tangentSample.y * up + tangentSample.z * N; 
 
+            // Accumulate irradiance: sample environment map and weight by angle.
+            // Angle weighting ensures that the sampling correctly captures the varying influence of light from different directions.
             irradiance += texture(environmentMap, sampleVec).rgb * cos(theta) * sin(theta);
             nrSamples++;
         }
     }
+
+    // Average accumulated irradiance and scale by PI. (this Pi here is a normalization step)
     irradiance = PI * irradiance * (1.0 / float(nrSamples));
     
     FragColor = vec4(irradiance, 1.0);
